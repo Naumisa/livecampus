383cb8c1c078d0e9c092c854d1d58b03e8a897d8
WAGNER Thomas <twagner@antropus.fr>
2024-03-01 15:29:49 +0100

    Fix on dashboard.php form
    - Remise en place du champ name qui n'était plus présente dans le formulaire d'envoi de fichiers.

--- resources/views/user/dashboard.php
+++ resources/views/user/dashboard.php
@@ -128,7 +128,7 @@
 								<?= lang_get('file_upload.upload_max_size') ?>
 							</p>
 						</div>
-						<input id="dropzone-file" type="file" class="hidden" />
+						<input id="dropzone-file" type="file" name="dropzone-file" class="hidden" />
 					</label>
 
 					<button data-modal-hide="upload-modal" type="submit" class="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
