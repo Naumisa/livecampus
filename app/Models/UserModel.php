83d48ac5471de4c9fa7b35d8d23f4df7a4fde5b0
WAGNER Thomas <twagner@antropus.fr>
2024-03-01 01:28:48 +0100

    Comments: UserModel.php
    - Ajout des commentaires de toutes les fonctions actuellement implémantées.

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -1,10 +1,16 @@
 <?php
 
+/**
+ * Retourne le nom de la table des utilisateurs.
+ * @return string Le nom de la table.
+ */
 function user_get_table (): string
 { return 'users'; }
 
 /**
- * @return array[]
+ * Définit la structure de la table des utilisateurs, y compris les types de données,
+ * les contraintes et les valeurs par défaut des champs.
+ * @return array La définition des champs de la table des utilisateurs.
  */
 function user_get_fields (): array
 {
@@ -61,11 +67,13 @@ function user_get_fields (): array
 }
 
 /**
- * @param string $username
- * @param string $email
- * @param string $password
- * @param int $role
- * @return array
+ * Prépare un tableau avec les données d'un utilisateur pour insertion ou mise à jour,
+ * en incluant le hachage du mot de passe.
+ * @param string $username Le nom d'utilisateur.
+ * @param string $email L'email de l'utilisateur.
+ * @param string $password Le mot de passe de l'utilisateur (sera haché).
+ * @param int $role Le rôle de l'utilisateur.
+ * @return array Le tableau des données de l'utilisateur.
  */
 function user_get_data_array(string $username, string $email, string $password, int $role): array
 {
@@ -78,6 +86,8 @@ function user_get_data_array(string $username, string $email, string $password, 
 }
 
 /**
+ * Crée ou met à jour la table des utilisateurs dans la base de données selon la
+ * structure définie dans user_get_fields.
  * @return void
  */
 function user_migrate(): void
@@ -86,8 +96,10 @@ function user_migrate(): void
 }
 
 /**
- * @param array $data
- * @return array|null
+ * Crée un nouvel utilisateur dans la base de données avec les données fournies.
+ * Retourne les données de l'utilisateur créé ou null en cas d'échec.
+ * @param array $data Les données de l'utilisateur à créer.
+ * @return array|null Les données de l'utilisateur créé ou null en cas d'échec.
  */
 function user_create(array $data): ?array
 {
@@ -103,21 +115,23 @@ function user_create(array $data): ?array
 }
 
 /**
- * @param int $id
- * @param array $data
+ * Met à jour les informations d'un utilisateur spécifié par son ID avec les données fournies.
+ * @param int $id L'ID de l'utilisateur à mettre à jour.
+ * @param array $data Les données à mettre à jour.
  * @return void
  */
 function user_update(int $id, array $data): void
 {
 	if (!db_update(user_get_table(), $id, $data))
 	{
-		log_file("Attempted to create a duplicate user.");
+		log_file("Error when attempted to update user($id).");
 	}
 }
 
 /**
- * @param int $id
- * @return array
+ * Récupère les informations d'un utilisateur spécifié par son ID.
+ * @param int $id L'ID de l'utilisateur.
+ * @return array Les données de l'utilisateur.
  */
 function user_get_data_with_id (int $id): array
 {
@@ -125,8 +139,9 @@ function user_get_data_with_id (int $id): array
 }
 
 /**
- * @param string $email
- * @return array
+ * Récupère les informations d'un utilisateur spécifié par son email.
+ * @param string $email L'email de l'utilisateur.
+ * @return array Les données de l'utilisateur.
  */
 function user_get_data_with_email (string $email): array
 {
@@ -134,9 +149,10 @@ function user_get_data_with_email (string $email): array
 }
 
 /**
- * @param int $id
- * @param string $password
- * @return bool
+ * Vérifie si le mot de passe fourni correspond au mot de passe de l'utilisateur authentifié.
+ * @param int $id L'ID de l'utilisateur.
+ * @param string $password Le mot de passe à vérifier.
+ * @return bool True si le mot de passe correspond, false sinon.
  */
 function user_confirm_password (int $id, string $password): bool
 {
@@ -145,8 +161,10 @@ function user_confirm_password (int $id, string $password): bool
 }
 
 /**
- * @return string
- * @throws \Random\RandomException
+ * Génère un sélecteur et un validateur pour un token.
+ * Utile pour les fonctionnalités d'authentification ou de réinitialisation de mot de passe.
+ * @return string Le token généré sous forme 'sélecteur:validateur'.
+ * @throws Exception Si la génération de bytes aléatoires échoue.
  */
 function user_generate_tokens(): string
 {
