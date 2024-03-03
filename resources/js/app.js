b79749b4219d3d6274fd76161ad0c570be412ddc
FrozenCell239 <val8ioks@gmail.com>
2024-03-03 19:10:38 +0100

    Bug fix - Fixed a bug that disable the password change form after cancelling user informations modifications.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,20 @@
+
 // Enabling user edit form
 function enableUserEditForm(){
-    const inputs = document.querySelectorAll('input');
-
-    inputs.forEach(function(input){
-        input.removeAttribute('disabled');
-    });
+    document.getElementById('username')
+        .removeAttribute('disabled')
+        .focus()
+    ;
+    document.getElementById('email').removeAttribute('disabled');
     document.getElementById('cancel').removeAttribute('hidden');
     document.getElementById('submit').removeAttribute('hidden');
     document.getElementById('edit').setAttribute('hidden', true);
-    inputs[0].focus();
 };
 
 // Disable user edit form (cancel editing)
 function disableUserEditForm(){
-    const inputs = document.querySelectorAll('input');
-
-    inputs.forEach(function(input){
-        input.setAttribute('disabled', true);
-    });
+    document.getElementById('username').setAttribute('disabled', true);
+    document.getElementById('email').setAttribute('disabled', true);
     document.getElementById('cancel').setAttribute('hidden', true);
     document.getElementById('submit').setAttribute('hidden', true);
     document.getElementById('edit').removeAttribute('hidden');
