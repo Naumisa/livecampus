b7336512513978c38c053973dbbf04b9594d4be5
WAGNER Thomas <twagner@antropus.fr>
2024-03-03 12:24:24 +0100

    footer.php
    - Ajout d'un footer affichant les liens accessibles (selon si connecté ou non).

--- resources/layouts/footer.php
+++ resources/layouts/footer.php
@@ -1,4 +1,28 @@
 <div>
-	<!-- Pied de page -->
+	<footer class="<?= isLoggedIn() ? 'md:ml-64' : 'md:mx-8' ?> bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+		<div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+			<span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <?= lang_get('title') ?>™. All Rights Reserved.
+			</span>
+			<ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+				<?php if (isLoggedIn()): ?>
+					<?php foreach($loggedLinks as $link => $value): ?>
+					<li>
+						<a href="<?= routes_go_to_route($link) ?>" class="hover:underline me-4 md:me-6">
+							<?= lang_get($value) ?>
+						</a>
+					</li>
+					<?php endforeach; ?>
+				<?php else: ?>
+					<?php foreach($loggedOutLinks as $link => $value): ?>
+						<li>
+							<a href="<?= routes_go_to_route($link) ?>" class="hover:underline me-4 md:me-6">
+								<?= lang_get($value) ?>
+							</a>
+						</li>
+					<?php endforeach; ?>
+				<?php endif; ?>
+			</ul>
+		</div>
+	</footer>
 
 </div>
